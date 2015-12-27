4e7c778e1a55e867985e1905e3c1f29cb2f8ade3
Alexander Petushkov <aleks.petushkov@hotmail.com>
2015-12-27 22:07:21 +0600

    begin

---
+++ virtual/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+
+class A
+{
+public:
+	void show()
+	{
+		cout << "Class A\n";
+	}
+};
+
+
+class B: public A
+{
+
+public:
+	virtual void show()
+	{
+		cout << "Class B\n";
+	}
+};
+
+class C: public A
+{
+
+public:
+	virtual void show()
+	{
+		cout << "Class C\n";
+	}
+	
+
+};
+
+int main()
+{
+	A* a;
+	a = new B;
+	a->show();
+
+	a = new C;
+	a->show();
+
+	cin.get();
+}
